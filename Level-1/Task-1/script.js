98e48fad2ec808a0c4cbd4aa50127c80e937714b
Sanjana Panchal <sanjanapanchal2713@gmail.com>
2025-07-16 16:48:04 +0530

    day 1

--- Level-1/Task-1/script.js
+++ Level-1/Task-1/script.js
@@ -1,6 +1,7 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.getElementById('navmenu');
 
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
