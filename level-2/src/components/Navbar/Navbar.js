38f5989891e192c600800cd74312997d85cce3b9
Sanjana Panchal <sanjanapanchal2713@gmail.com>
2025-07-22 21:08:21 +0530

    update

---
+++ level-2/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <nav></nav>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
