16ccb9ca3d0e88766dd2c9f18e3bd7f09474e692
Sanjana Panchal <sanjanapanchal2713@gmail.com>
2025-07-23 21:14:40 +0530

    update

--- level-2/src/App.js
+++ level-2/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
-    <>
+    
+    <Router> 
       <Navbar />
-    </>
+    </Router>
   );
 }
 
--- level-2/src/components/Navbar/Navbar.js
+++ level-2/src/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import '../../styles/Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <nav></nav>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/">About</NavLink>
+              <NavLink to="/">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
         <h1>Hello</h1>
       </div>
     </>
