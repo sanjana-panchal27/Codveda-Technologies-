8765a38cde46576ec59721800e9e2cc6def88bb8
Sanjana Panchal <sanjanapanchal2713@gmail.com>
2025-07-27 22:02:04 +0530

    Attempted Tailwind CSS setup, facing 'command not found' error

--- level-2/src/App.js
+++ level-2/src/App.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Testimonial from './components/Testimonial';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import { AppProvider } from './context/AppContext';
-import Navbar from './components/Navbar/Navbar';
+// import Hero from './components/Hero';
+// import Features from './components/Features';
+// import Testimonial from './components/Testimonial';
+// import ContactForm from './components/ContactForm';
+// import Footer from './components/Footer';
+// import { AppProvider } from './context/AppContext';
+// import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <AppProvider>
-        <Navbar />
+    // <Router>
+    //   <AppProvider>
+    //     <Navbar />
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Features />
-                <Testimonial />
-                <ContactForm />
-              </>
-            }
-          />
-        </Routes>
+    //     <Routes>
+    //       <Route
+    //         path="/"
+    //         element={
+    //           <>
+    //             <Hero />
+    //             <Features />
+    //             <Testimonial />
+    //             <ContactForm />
+    //           </>
+    //         }
+    //       />
+    //     </Routes>
 
-        <Footer />
-      </AppProvider>
-    </Router>
+    //     <Footer />
+    //   </AppProvider>
+    // </Router>
+    <>
+      <h1 className="text-7xl text-center">Hello world</h1>
+    </>
   );
 }
 
