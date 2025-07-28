856e27bfcd0326a2376975c6ba09cc3ce88a39db
Sanjana Panchal <sanjanapanchal2713@gmail.com>
2025-07-28 21:56:35 +0530

    CSS added

--- level-2/src/components/Navbar/Navbar.js
+++ level-2/src/components/Navbar/Navbar.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import '../../styles/Navbar.css';
 import { NavLink } from 'react-router-dom';
-<script src="https://cdn.tailwindcss.com"></script>
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        <NavLink to="#" className="text-2xl font-bold">
+    <nav className="navbar">
+      <div className="navbar-container">
+        <NavLink to="#" className="navbar-brand">
           MyBrand
         </NavLink>
-        <div className="space-x-4">
-          <NavLink to="#hero" className="hover:text-gray-300">
+
+        <div className="navbar-links">
+          <NavLink to="#hero" className="navbar-link">
             Home
           </NavLink>
-          <NavLink to="#features" className="hover:text-gray-300">
+          <NavLink to="#features" className="navbar-link">
             Features
           </NavLink>
-          <NavLink to="#testimonial" className="hover:text-gray-300">
+          <NavLink to="#testimonial" className="navbar-link">
             Testimonials
           </NavLink>
-          <NavLink to="#contact" className="hover:text-gray-300">
+          <NavLink to="#contact" className="navbar-link">
             Contact
           </NavLink>
         </div>
